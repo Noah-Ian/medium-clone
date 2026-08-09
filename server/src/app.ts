8b24ff164cea7cce9286583787f192eed3b7b5ca
Noah-Ian <noahian140@gmail.com>
2026-08-09 22:48:56 +0300

    added auth route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,15 @@
 import express from "express";
-//import router from "./routes/user.routes.js";
-import router from "./routes/auth.routes.js";
+import router from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-//app.use("/api/users", router);
+app.use("/api/users", router);
 
 app.use("/api/auth",
-    router
+    authRoutes
 );
 
 export default app;
